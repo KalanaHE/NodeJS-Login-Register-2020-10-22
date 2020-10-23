7f66e2dc1f0a53a976a5a538c922b29946e64b29
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-10-23 13:58:49 +0530

    signup request validated with validation middleware(express-validator)

--- src/validators/signup.validator.js
+++ src/validators/signup.validator.js
@@ -7,9 +7,9 @@ exports.signupvalidator = [
   check("username").notEmpty().withMessage("Username is required!"),
 
   check("email").notEmpty().withMessage("Email is required!"),
-  check("password").notEmpty().withMessage("Password is required!"),
-
   check("email").isEmail().withMessage("Enter a valid email!"),
+
+  check("password").notEmpty().withMessage("Password is required!"),
   check("password")
     .isLength({ min: 4 })
     .withMessage("Password must be at least 2 characters long!"),
@@ -19,7 +19,7 @@ exports.isRequestValidated = (req, res, next) => {
   const errors = validationResult(req);
 
   if (errors.array().length > 0) {
-    return response(req, res, 400, { info: errors.array()[0].msg }, {});
+    return response(req, res, 400, {}, { info: errors.array()[0].msg });
   } else {
     next();
   }
